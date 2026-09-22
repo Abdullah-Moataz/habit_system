f1832cb4aaaa16406d3206b4d0fe89550f0ab1d3
Abdullah Moataz <a.moatazbellah4@gmail.com>
2026-09-22 14:03:19 +0300

    Update habit.py

--- habit.py
+++ habit.py
@@ -35,7 +35,8 @@ if "hdf" not in st.session_state:
 #load API LLm
 load_dotenv()
 
-api_key = os.getenv("OPENROUTER_API_KEY")
+api_key = st.secrets("OPENROUTER_API_KEY")
+
 
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
